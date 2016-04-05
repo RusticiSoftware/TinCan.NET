05b1d8458646f7202b7a1c21be94b35364a86fc7
Brian J. Miller <brian.miller@scorm.com>
2016-04-05 13:48:44 -0600

    Release 1.0.0.0

--- TinCan/Properties/AssemblyInfo.cs
+++ TinCan/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TinCan")]
-[assembly: AssemblyDescription("Library for implementing Tin Can API")]
+[assembly: AssemblyDescription("Library for implementing Tin Can API (Experience API)")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Rustici Software")]
 [assembly: AssemblyProduct("TinCan.NET")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.6.0")]
-[assembly: AssemblyFileVersion("0.0.6.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
